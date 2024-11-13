9788b8c1ca03640c240dd1c60793514295010dde
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-11-13 15:50:08 +0100

    fixed bug in BulletCCScript -> damage after death
    ~ now, zombie cannot get damage after death.

--- Assets/Easy FPS CC/Scripts/BulletCCScript.cs
+++ Assets/Easy FPS CC/Scripts/BulletCCScript.cs
@@ -2,25 +2,26 @@
 using System.Collections;
 using CodeMonkey.HealthSystemCM;
 
-public class BulletCCScript : MonoBehaviour {
+public class BulletCCScript : MonoBehaviour
+{
 
-	[Tooltip("Furthest distance bullet will look for target")]
-	public float maxDistance = 1000000;
-	RaycastHit hit;
-	[Tooltip("Prefab of wall damange hit. The object needs 'LevelPart' tag to create decal on it.")]
-	public GameObject decalHitWall;
-	[Tooltip("Decal will need to be sligtly infront of the wall so it doesnt cause rendeing problems so for best feel put from 0.01-0.1.")]
-	public float floatInfrontOfWall;
+    [Tooltip("Furthest distance bullet will look for target")]
+    public float maxDistance = 1000000;
+    RaycastHit hit;
+    [Tooltip("Prefab of wall damange hit. The object needs 'LevelPart' tag to create decal on it.")]
+    public GameObject decalHitWall;
+    [Tooltip("Decal will need to be sligtly infront of the wall so it doesnt cause rendeing problems so for best feel put from 0.01-0.1.")]
+    public float floatInfrontOfWall;
     [Tooltip("Name of layer for Walls")]
     [SerializeField]
     private string wallLayerName;
-	[Tooltip("Blood prefab particle this bullet will create upoon hitting enemy")]
-	public GameObject bloodEffect;
+    [Tooltip("Blood prefab particle this bullet will create upoon hitting enemy")]
+    public GameObject bloodEffect;
     [Tooltip("Name of layer for characters who can bleed")]
-	[SerializeField]
+    [SerializeField]
     private string charactersLayerName;
     [Tooltip("Put Weapon layer and Player layer to ignore bullet raycast.")]
-	public LayerMask ignoreLayer;
+    public LayerMask ignoreLayer;
 
     public float damage = 1;
 
@@ -29,32 +30,40 @@ public class BulletCCScript : MonoBehaviour {
 	* bullet creates a raycast which searches for corresponding tags.
 	* If raycast finds somethig it will create a decal of corresponding tag.
 	*/
-    void Update () {
+    void Update()
+    {
 
-		if(Physics.Raycast(transform.position, transform.forward,out hit, maxDistance, ~ignoreLayer)){
-			if(decalHitWall){
-				if(hit.collider.gameObject.layer == LayerMask.NameToLayer(wallLayerName)){
-					Instantiate(decalHitWall, hit.point + hit.normal * floatInfrontOfWall, Quaternion.LookRotation(hit.normal));
-					Destroy(gameObject);
-				}
-			}
-			if (bloodEffect)
-			{
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer(charactersLayerName))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, ~ignoreLayer))
+        {
+            if (decalHitWall)
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer(wallLayerName))
                 {
-                    Instantiate(bloodEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                    hit.collider.gameObject.GetComponent<HealthSystemComponent>()?.GetHealthSystem().Damage(damage);
+                    Instantiate(decalHitWall, hit.point + hit.normal * floatInfrontOfWall, Quaternion.LookRotation(hit.normal));
                     Destroy(gameObject);
                 }
             }
-			Destroy(gameObject);
-		}
-		Destroy(gameObject, 0.1f);
-	}
+            if (bloodEffect)
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer(charactersLayerName))
+                {
+                    Instantiate(bloodEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                    var hitHealthSystem = hit.collider.gameObject.GetComponent<HealthSystemComponent>();
+                    if (hitHealthSystem != null && hitHealthSystem.GetHealthSystem().IsDead() == false)
+                    {
+                        hitHealthSystem.GetHealthSystem().Damage(damage);
+                    }
+                    Destroy(gameObject);
+                }
+            }
+            Destroy(gameObject);
+        }
+        Destroy(gameObject, 0.1f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+
     }
 
 }
