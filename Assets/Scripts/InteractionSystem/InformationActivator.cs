0ce95573ee4976181ece828355d03e272a2e871d
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2025-03-05 22:25:54 +0100

    Generic InformationActivator created and InformationTextActivator refactored

---
+++ Assets/Scripts/InteractionSystem/InformationActivator.cs
@@ -0,0 +1,30 @@
+using InteractionSystem.Interfaces;
+using UnityEngine;
+
+public class InformationActivator<T> : Activator<IInformable<T>>, IDeactivator
+{
+    [SerializeField]
+    Component displayable;
+    IDisplayable<T> _displayable;
+
+    [SerializeField]
+    Component hideable;
+    IHideable _hideable;
+
+    protected new void Awake()
+    {
+        base.Awake();
+        _displayable = displayable.GetComponent<IDisplayable<T>>();
+        _hideable = hideable.GetComponent<IHideable>();
+    }
+
+    protected override void Activation(IInformable<T> obj)
+    {
+        _displayable.Display(obj.Inform());
+    }
+
+    public void Deactivate()
+    {
+        _hideable.Hide();
+    }
+}
--- Assets/Scripts/InteractionSystem/InformationTextActivator.cs
+++ Assets/Scripts/InteractionSystem/InformationTextActivator.cs
@@ -1,45 +1,12 @@
-using InteractionSystem.Interfaces;
-using UnityEngine;
-//using UnityEngine.Events;
 
-public class InformationTextActivator : Activator<IInformable<string>>, IDeactivator
+public class InformationTextActivator : InformationActivator<string>
 {
-    [SerializeField]
-    Component displayable;
-    IDisplayable<string> _displayable;
-
-    [SerializeField]
-    Component hideable;
-    IHideable _hideable;
-
-    /*[SerializeField]
-    protected UnityEvent<IInformable<string>> objectActivated;*/
-
-    protected new void Awake()
-    {
-        base.Awake();
-        _displayable = displayable.GetComponent<IDisplayable<string>>();
-        _hideable = hideable.GetComponent<IHideable>();
-    }
-
     // Update is called once per frame
     protected void Update()
     {
         if (Activate() == false)
         {
             Deactivate();
-            //objectActivated?.Invoke(null);
         }
-    }
-
-    protected override void Activation(IInformable<string> obj)
-    {
-        _displayable.Display(obj.Inform());
-        //objectActivated?.Invoke(obj);
-    }
-
-    public void Deactivate()
-    {
-        _hideable.Hide();
     }
 }
